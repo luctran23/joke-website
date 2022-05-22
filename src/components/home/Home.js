7dd7a6cbaf523f91f5c4c4d9eae525e807bd02da
luctran23 <anhvanluc1999@gmail.com>
2022-05-22 07:36:31 +0700

    set cookies to save voted jokes

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,32 +1,55 @@
 import './index.css';
 import LeftLogo from '../../assets/images/logo.jpg';
 import RightLogo from '../../assets/images/right_logo.jpg';
-import { mockJokes } from './services/mock-data';
+import { mockJokes, footerData } from './services/mock-data';
 import { useEffect, useState } from 'react';
 
 const Home = () => {
     const [jokes, setJokes] = useState(mockJokes);
-    const [selectedJoke, setSelectedJoke] = useState(mockJokes[0]);
+    const [selectedJoke, setSelectedJoke] = useState(null);
+    const [votedJokes, setVotedJokes] = useState([]);
+
+    const setCurrentJoke = (jokes) => {
+        const unVotedJoke = jokes.find(joke => !joke.isVoted);
+        setSelectedJoke(unVotedJoke);
+    }
+
+    const getSavedJokes = (cookies) => {
+        const arrayOfCookies = cookies.split('=');
+        const jokes = JSON.parse(arrayOfCookies[arrayOfCookies.length - 1]);
+        return jokes;
+    }
 
     useEffect(() => {
-        const unVotedJoke = jokes.find(joke => joke.isFunny === null);
-        setSelectedJoke(unVotedJoke);
+        if (document.cookie.length !== 0) {
+            const savedVotedJokes = getSavedJokes(document.cookie);
+            setVotedJokes(savedVotedJokes);
+            for (let i = 0; i < savedVotedJokes.length; i++) {
+                const joke = jokes.find(item => item.id === savedVotedJokes[i]);
+                joke.isVoted = true;
+            }
+            setJokes(jokes);
+            setCurrentJoke(jokes);
+        } else {
+            setCurrentJoke(jokes)
+        }
     }, []);
 
-    const handleFunny = () => {
-        const votedJokeIndex = jokes.indexOf(selectedJoke)
+    const handleFunny = (isFunny) => {
+        const votedJokeIndex = jokes.indexOf(selectedJoke);
         if (votedJokeIndex !== -1) {
-            jokes[votedJokeIndex].isFunny = true;
+            votedJokes.push(selectedJoke.id);
+            setVotedJokes(votedJokes);
+            const expireTime = new Date();
+            expireTime.setHours(23,59,59);
+            document.cookie = `voted_jokes=${JSON.stringify(votedJokes)}; expires=${expireTime}; path=/`;
+            jokes[votedJokeIndex].isVoted = true;
             setJokes(jokes);
-            const unVotedJoke = jokes.find(joke => joke.isFunny === null);
+            const unVotedJoke = jokes.find(joke => !joke.isVoted);
             setSelectedJoke(unVotedJoke);
         }
-        console.log('selected joke', selectedJoke, votedJokeIndex)
     }
 
-    const handleNotFunny = () => {
-
-    }
     return (
         <>
             <div className='container'>
@@ -44,17 +67,14 @@ const Home = () => {
                     </p>
                 </div>
                 <div className='btn__group'>
-                    <button className='btn__primary' onClick={handleFunny}>This is funny</button>
-                    <button className='btn__info' onClick={handleNotFunny}>This is not funny</button>
+                    <button className='btn__primary' onClick={() => handleFunny(true)}>This is funny</button>
+                    <button className='btn__info' onClick={() => handleFunny(false)}>This is not funny</button>
                 </div>
                 <div className='footer'>
                     <p>
-                        This website is created as apart of HLsolutions program. The materials contained on this website are provided
-                        for general information only and do not constitute any form of advice. HLS assumes no responsibility for the accuracy
-                        of any particular statement and accepts no liability for any loss or damage which may be arise from reliance on the
-                        information contained on this site.
+                        {footerData.description}
                     </p>
-                    <p className='copyright'>Copyright 2021 HLS</p>
+                    <p className='copyright'>{footerData.title}</p>
                 </div>
             </div>
         </>
--- src/components/home/services/mock-data.js
+++ src/components/home/services/mock-data.js
@@ -2,6 +2,7 @@ export const mockJokes = [
     {
         id: 1,
         isFunny: null,
+        isVoted: false,
         value: `A child asked his father, "How were people born?" So his father said, "Adam and Eve made babies, then their babies became adults and made babies, and so on."
 
         The child then went to his mother, asked her the same question and she told him, "We were monkeys then we evolved to become like we are now."
@@ -11,16 +12,27 @@ export const mockJokes = [
     {
         id: 2,
         isFunny: null,
+        isVoted: false,
         value: `Teacher: "Kids,what does the chicken give you?" Student: "Meat!" Teacher: "Very good! Now what does the pig give you?" Student: "Bacon!" Teacher: "Great! And what does the fat cow give you?" Student: "Homework!"`
     },
     {
         id: 3,
         isFunny: null,
+        isVoted: false,
         value: `The teacher asked Jimmy, "Why is your cat at school today Jimmy?" Jimmy replied crying, "Because I heard my daddy tell my mommy, 'I am going to eat that pussy once Jimmy leaves for school today!'"`
     },
     {
         id: 4,
         isFunny: null,
+        isVoted: false,
         value: `A housewife, an accountant and a lawyer were asked "How much is 2+2?" The housewife replies: "Four!". The accountant says: "I think it's either 3 or 4. Let me run those figures through my spreadsheet one more time." The lawyer pulls the drapes, dims the lights and asks in a hushed voice, "How much do you want it to be?"`
     },
-]
+];
+
+export const footerData = {
+    description: `This website is created as apart of HLsolutions program. The materials contained on this website are provided
+    for general information only and do not constitute any form of advice. HLS assumes no responsibility for the accuracy
+    of any particular statement and accepts no liability for any loss or damage which may be arise from reliance on the
+    information contained on this site.`,
+    title: `Copyright 2021 HLS`
+}
